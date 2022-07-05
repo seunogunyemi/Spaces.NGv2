c24bae0e23ef05893ca6c99307b7883eedc4e486
seunogunyemi <timothyogunyemi@gmail.com>
2022-07-05 17:01:10 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SpacesNGv2
---
+++ js/spaces.js
@@ -0,0 +1,8 @@
+// const control = document.getElementsByid('featured');
+// const visible_onHover = document.getElementsByid('visible_onHover');
+// control.addEventlistener('mouseover', function handleMouseOver(){
+//     visible_onHover.style.display = 'block';
+// })
+// control.addEventlistener('mouseout', function handleMouseOut(){
+//     visible_onHover.style.display = 'none';
+// })
